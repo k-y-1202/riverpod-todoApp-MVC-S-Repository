732c33675ec5dd58cd72dfc8ed1c7f83abe328a8
nobu <hiroki-pon@outlook.jp>
2023-04-13 16:08:47 +0900

    firebaseの認証用のprovider

---
+++ lib/src/features/auth/repository/auth_repository.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'auth_repository.g.dart';
+
+class AuthRepository {
+  AuthRepository(this._auth);
+  final FirebaseAuth _auth;
+
+  Stream<User?> authStateChanges() => _auth.authStateChanges();
+  User? get currentUser => _auth.currentUser;
+}
+
+@Riverpod(keepAlive: true)
+FirebaseAuth firebaseAuth(FirebaseAuthRef ref) {
+  return FirebaseAuth.instance;
+}
+
+@Riverpod(keepAlive: true)
+AuthRepository authRepository(AuthRepositoryRef ref) {
+  return AuthRepository(ref.watch(firebaseAuthProvider));
+}
+
+@riverpod
+Stream<User?> authStateChanges(AuthStateChangesRef ref) {
+  return ref.watch(authRepositoryProvider).authStateChanges();
+}
