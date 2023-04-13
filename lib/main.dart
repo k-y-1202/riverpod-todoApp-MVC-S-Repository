97ededa0d5ca90ebcb9dcd8daa1cb4a112cfcf70
nobu <hiroki-pon@outlook.jp>
2023-04-13 13:52:09 +0900

    ルーティングを追加

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,18 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_todo_app_mvc_s_repository/firebase_options.dart';
+import 'package:riverpod_todo_app_mvc_s_repository/src/config/routing/app_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Firebaseの初期化
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(
+    // ProviderScopeの内側でRiverpodのProviderが使用できる
     const ProviderScope(
       child: MyApp(),
     ),
@@ -20,12 +25,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false, // debug用のバナーを非表示
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const TestScreen(),
+      routerConfig: AppRouter.router,
     );
   }
 }
