d1dcb951b7995030788113b637d4b928c550faa9
nobu <hiroki-pon@outlook.jp>
2023-04-20 12:30:21 +0900

    不要なimport文の削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_todo_app_mvc_s_repository/firebase_options.dart';
