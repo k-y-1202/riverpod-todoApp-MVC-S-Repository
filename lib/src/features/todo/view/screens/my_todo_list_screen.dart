74a5f9544f992eccdc3f581ab5f17e71fc5305e7
nobu <hiroki-pon@outlook.jp>
2023-04-20 15:56:41 +0900

    自分だけのタスクの表示とタスク追加画面への遷移

--- lib/src/features/todo/view/screens/my_todo_list_screen.dart
+++ lib/src/features/todo/view/screens/my_todo_list_screen.dart
@@ -1,28 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_todo_app_mvc_s_repository/src/config/routing/route_utils.dart';
+import 'package:riverpod_todo_app_mvc_s_repository/src/features/todo/controller/team_todo_list_controller.dart';
 import 'package:riverpod_todo_app_mvc_s_repository/src/features/todo/data_model/todo.dart';
 import 'package:riverpod_todo_app_mvc_s_repository/src/features/todo/view/component/todo_list_builder.dart';
 
-class MyTodoListScreen extends StatelessWidget {
+class MyTodoListScreen extends ConsumerWidget {
   const MyTodoListScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // TODO: ここで、自分のTodoリストを取得する
+    final provider = ref.watch(teamTodoListControllerProvider.notifier);
     return Scaffold(
       appBar: AppBar(
         title: const Text('自分だけのタスク'),
       ),
       body: TodoListBuilder(
-        todoList: [
-          Todo(
-            isDone: false,
-            title: 'hoge',
-            todoId: '',
-            userId: '',
-            createdAt: '',
-          ),
-        ],
+        stream: provider.teamTodoList(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
