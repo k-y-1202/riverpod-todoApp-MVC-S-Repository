66e9c7b234ca333b22da522dd3723e134b3ee91c
nobu <hiroki-pon@outlook.jp>
2023-04-20 15:55:09 +0900

    team_todo_list_stateを削除

--- lib/src/features/todo/controller/team_todo_list_controller.dart
+++ lib/src/features/todo/controller/team_todo_list_controller.dart
@@ -1,33 +1,15 @@
-import 'dart:ui';
-
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:riverpod_todo_app_mvc_s_repository/src/features/todo/controller/state/team_todo_list_state.dart';
 import 'package:riverpod_todo_app_mvc_s_repository/src/features/todo/data_model/todo.dart';
+import 'package:riverpod_todo_app_mvc_s_repository/src/features/todo/repository/todo_repository.dart';
 
 part 'team_todo_list_controller.g.dart';
 
 @riverpod
 class TeamTodoListController extends _$TeamTodoListController {
-  TeamTodoListController() : super() {
-    Future(() async {
-      state = state.copyWith(isLoading: true);
-      // TODO: todoリストを取得する
-      await Future.delayed(
-        const Duration(seconds: 3),
-      );
-      state = state.copyWith(isLoading: false);
-    });
-  }
-
   @override
-  TeamTodoListState build() {
-    Todo todo = Todo(
-      todoId: "todoId",
-      userId: "userId",
-      title: "title",
-      isDone: false,
-    );
-    return TeamTodoListState(todoList: [todo, todo]);
+  build() {}
+
+  Stream<List<Todo>> teamTodoList() {
+    return ref.read(todoRepoProvider).getTeamTodoList(teamId: "teamId");
   }
 }
--- lib/src/features/todo/controller/team_todo_list_controller.g.dart
+++ lib/src/features/todo/controller/team_todo_list_controller.g.dart
@@ -7,12 +7,12 @@ part of 'team_todo_list_controller.dart';
 // **************************************************************************
 
 String _$teamTodoListControllerHash() =>
-    r'6b9894b8154a0eb31b4bd8c3b33cfd5f521a4574';
+    r'77e9b3e5ade17b4b1f7d7b36f036926cd49293fb';
 
 /// See also [TeamTodoListController].
 @ProviderFor(TeamTodoListController)
-final teamTodoListControllerProvider = AutoDisposeNotifierProvider<
-    TeamTodoListController, TeamTodoListState>.internal(
+final teamTodoListControllerProvider =
+    AutoDisposeNotifierProvider<TeamTodoListController, dynamic>.internal(
   TeamTodoListController.new,
   name: r'teamTodoListControllerProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -22,5 +22,5 @@ final teamTodoListControllerProvider = AutoDisposeNotifierProvider<
   allTransitiveDependencies: null,
 );
 
-typedef _$TeamTodoListController = AutoDisposeNotifier<TeamTodoListState>;
+typedef _$TeamTodoListController = AutoDisposeNotifier<dynamic>;
 // ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
