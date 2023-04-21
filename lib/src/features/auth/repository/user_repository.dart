3d62a13b209927b14155f5b543e36189645cc49c
nobu <hiroki-pon@outlook.jp>
2023-04-21 10:49:01 +0900

    user_repositoryを作成

---
+++ lib/src/features/auth/repository/user_repository.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_todo_app_mvc_s_repository/src/config/utils/firebase_provider.dart';
+import 'package:riverpod_todo_app_mvc_s_repository/src/config/utils/keys.dart';
+import 'package:riverpod_todo_app_mvc_s_repository/src/features/auth/data_model/user.dart';
+
+final userFirestoreProvider = Provider<CollectionReference<User>>(
+  (ref) => ref
+      .read(firestoreProvider)
+      .collection(Keys.userCollection)
+      .withConverter(
+        fromFirestore: (snapshot, options) => User.fromJson(snapshot.data()!),
+        toFirestore: (value, options) => value.toJson(),
+      ),
+);
+
+final userRepoProvider = Provider<UserRepository>(
+  (ref) => UserRepository(ref: ref),
+);
+
+class UserRepository {
+  UserRepository({required this.ref}) : _db = ref.read(userFirestoreProvider);
+  final Ref ref;
+  final CollectionReference<User> _db;
+
+  // create
+  Future<void> addUser({
+    required String userName,
+    required String email,
+  }) async {
+    final doc = _db.doc();
+    final user = User(
+      userName: userName,
+      userId: doc.id,
+      email: email,
+      createdAt: DateTime.now().toIso8601String(),
+    );
+    await doc.set(user);
+  }
+
+  // update
+  Future<void> updateUser({
+    required String userId,
+    required String userName,
+  }) async =>
+      await _db.doc(userId).update({'userName': userName});
+
+  // read
+  Future<User?> getUser({required String userId}) async =>
+      await _db.doc(userId).get().then((doc) => doc.data());
+}
