6860ba4cf1e48b24aa2ad2a5083438978473281f
nobu <hiroki-pon@outlook.jp>
2023-04-21 10:49:19 +0900

    styleNameクラスを作成

---
+++ lib/src/config/utils/styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_todo_app_mvc_s_repository/src/config/utils/colors.dart';
+
+class StyleName {
+  static const title = TextStyle(
+    fontSize: 16,
+  );
+  static const description = TextStyle(
+    fontSize: 12,
+    color: ColorName.grey,
+  );
+}
