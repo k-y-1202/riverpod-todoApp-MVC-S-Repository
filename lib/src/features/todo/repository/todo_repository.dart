49257e59917ec748afe845c27958815d1407490c
nobu <hiroki-pon@outlook.jp>
2023-04-21 10:25:21 +0900

    修正

--- lib/src/features/todo/repository/todo_repository.dart
+++ lib/src/features/todo/repository/todo_repository.dart
@@ -4,18 +4,14 @@ import 'package:riverpod_todo_app_mvc_s_repository/src/config/utils/keys.dart';
 import 'package:riverpod_todo_app_mvc_s_repository/src/features/auth/repository/auth_repository.dart';
 import 'package:riverpod_todo_app_mvc_s_repository/src/features/todo/data_model/todo.dart';
 
-final todoFirestoreProvider = Provider((ref) {
-  return FirebaseFirestore.instance
-      .collection(Keys.todoCollection)
-      .withConverter(
-        fromFirestore: (snapshot, options) => Todo.fromJson(snapshot.data()!),
-        toFirestore: (value, options) => value.toJson(),
-      );
-});
+final todoFirestoreProvider = Provider((ref) =>
+    FirebaseFirestore.instance.collection(Keys.todoCollection).withConverter(
+          fromFirestore: (snapshot, options) => Todo.fromJson(snapshot.data()!),
+          toFirestore: (value, options) => value.toJson(),
+        ));
 
-final todoRepoProvider = Provider<TodoRepository>((ref) {
-  return TodoRepository(ref: ref);
-});
+final todoRepoProvider =
+    Provider<TodoRepository>((ref) => TodoRepository(ref: ref));
 
 class TodoRepository {
   TodoRepository({required this.ref}) : _db = ref.read(todoFirestoreProvider);
@@ -42,28 +38,22 @@ class TodoRepository {
   Future<void> updateTodo({
     required String todoId,
     required bool isDone,
-  }) async {
-    await _db.doc(todoId).update({'isDone': isDone});
-  }
+  }) async =>
+      await _db.doc(todoId).update({'isDone': isDone});
 
   // read
-  Stream<List<Todo>> getMyTodoList({required String userId}) {
-    return queryTodo().where(userId, isEqualTo: Keys.userId).snapshots().map(
-          (snapshot) => snapshot.docs.map((doc) => doc.data()).toList(),
-        );
-  }
+  Stream<List<Todo>> getMyTodoList({required String userId}) =>
+      queryTodo().where(userId, isEqualTo: Keys.userId).snapshots().map(
+            (snapshot) => snapshot.docs.map((doc) => doc.data()).toList(),
+          );
 
-  Stream<List<Todo>> getTeamTodoList({required String teamId}) {
-    return queryTodo().where(teamId, isEqualTo: Keys.teamId).snapshots().map(
-          (snapshot) => snapshot.docs.map((doc) => doc.data()).toList(),
-        );
-  }
+  Stream<List<Todo>> getTeamTodoList({required String teamId}) =>
+      queryTodo().where(teamId, isEqualTo: Keys.teamId).snapshots().map(
+            (snapshot) => snapshot.docs.map((doc) => doc.data()).toList(),
+          );
 
-  Query<Todo> queryTodo() {
-    Query<Todo> query = _db.withConverter(
-      fromFirestore: (snapshot, options) => Todo.fromJson(snapshot.data()!),
-      toFirestore: (value, options) => value.toJson(),
-    );
-    return query;
-  }
+  Query<Todo> queryTodo() => _db.withConverter(
+        fromFirestore: (snapshot, options) => Todo.fromJson(snapshot.data()!),
+        toFirestore: (value, options) => value.toJson(),
+      );
 }
