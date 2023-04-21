6de1c9cd1a2ba1348a3b1127b978035f17411464
nobu <hiroki-pon@outlook.jp>
2023-04-21 11:07:10 +0900

    hooksでページ遷移を管理

--- lib/src/features/navigation/view/bottom_navigation.dart
+++ lib/src/features/navigation/view/bottom_navigation.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:riverpod_todo_app_mvc_s_repository/src/config/routing/route_utils.dart';
 
-class BottomNavigation extends StatelessWidget {
+class BottomNavigation extends HookWidget {
   const BottomNavigation({
     super.key,
     required this.child,
@@ -14,34 +15,39 @@ class BottomNavigation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final currentIndex = useState<int>(0); // HooksのStateを使ってページ遷移を管理
+
     return Scaffold(
       body: child,
       bottomNavigationBar: BottomNavigationBar(
-        items: [
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
-            label: 'Home',
+            label: 'みんな',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
+            icon: Icon(Icons.my_library_books_outlined),
+            label: '自分だけ',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
+            icon: Icon(Icons.person),
+            label: 'マイページ',
           ),
         ],
-        currentIndex: 0,
+        currentIndex: currentIndex.value,
         selectedItemColor: Colors.amber[800],
         onTap: (index) {
           switch (index) {
             case 0:
+              currentIndex.value = 0;
               context.go(AppPage.teamTodoList.toPath);
               break;
             case 1:
+              currentIndex.value = 1;
               context.go(AppPage.myTodoList.toPath);
               break;
             case 2:
+              currentIndex.value = 2;
               context.go(AppPage.profile.toPath);
               break;
           }
