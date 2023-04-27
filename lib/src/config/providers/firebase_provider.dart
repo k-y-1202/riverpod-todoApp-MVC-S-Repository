772e47686edf93c6458ba7e172eee006261afa62
nobu <hiroki-pon@outlook.jp>
2023-04-27 14:10:28 +0900

    firebase関係のprovider

---
+++ lib/src/config/providers/firebase_provider.dart
@@ -0,0 +1,15 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+part 'firebase_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+FirebaseFirestore firestore(FirestoreRef ref) {
+  return FirebaseFirestore.instance;
+}
+
+@Riverpod(keepAlive: true)
+FirebaseAuth firebaseAuth(FirebaseAuthRef ref) {
+  return FirebaseAuth.instance;
+}
---
+++ lib/src/config/providers/firebase_provider.g.dart
@@ -0,0 +1,37 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'firebase_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$firestoreHash() => r'e271f8f7be694ee819f397232f4d140c15a5f921';
+
+/// See also [firestore].
+@ProviderFor(firestore)
+final firestoreProvider = Provider<FirebaseFirestore>.internal(
+  firestore,
+  name: r'firestoreProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$firestoreHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FirestoreRef = ProviderRef<FirebaseFirestore>;
+String _$firebaseAuthHash() => r'46c40b7c5cf8ab936c0daa96a6af106bd2ae5d51';
+
+/// See also [firebaseAuth].
+@ProviderFor(firebaseAuth)
+final firebaseAuthProvider = Provider<FirebaseAuth>.internal(
+  firebaseAuth,
+  name: r'firebaseAuthProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$firebaseAuthHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FirebaseAuthRef = ProviderRef<FirebaseAuth>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
