a69307ba46bf6b2a9ddee2b5875b4a686054f38c
nobu <hiroki-pon@outlook.jp>
2023-04-27 14:15:46 +0900

    認証用のserviceを作成

---
+++ lib/src/features/auth/service/sign_in_up_service.dart
@@ -0,0 +1,58 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:riverpod_todo_app_mvc_s_repository/src/config/providers/firebase_provider.dart';
+import 'package:riverpod_todo_app_mvc_s_repository/src/features/auth/data_model/user.dart';
+import 'package:riverpod_todo_app_mvc_s_repository/src/features/auth/repository/user_repository.dart';
+
+part 'sign_in_up_service.g.dart';
+
+@riverpod
+SignInUpService signInUpService(SignInUpServiceRef ref) {
+  return SignInUpService(ref: ref);
+}
+
+class SignInUpService {
+  SignInUpService({required this.ref})
+      : _auth = ref.read(firebaseAuthProvider),
+        _userRepo = ref.read(userRepoProvider);
+
+  final Ref ref;
+  final auth.FirebaseAuth _auth;
+  final UserRepository _userRepo;
+
+  // ログイン・新規登録
+  Future<void> signInUp({
+    required bool isRegister,
+    required String email,
+    required String pass,
+  }) async {
+    final userCredential = isRegister
+        ? await _auth.createUserWithEmailAndPassword(
+            email: email,
+            password: pass,
+          )
+        : await _auth.signInWithEmailAndPassword(
+            email: email,
+            password: pass,
+          );
+    final firebaseUser = _auth.currentUser;
+    if (firebaseUser != null) {
+      final isUserExistedInDb = await _userRepo.searchUserById(
+        userId: firebaseUser.uid,
+      );
+      if (!isUserExistedInDb) {
+        await _userRepo.addUser(user: _convertToUser(firebaseUser));
+      }
+    }
+  }
+
+  User _convertToUser(auth.User firebaseUser) {
+    return User(
+      userId: firebaseUser.uid,
+      userName: firebaseUser.displayName ?? "",
+      email: firebaseUser.email ?? "",
+      createdAt: DateTime.now().toIso8601String(),
+    );
+  }
+}
---
+++ lib/src/features/auth/service/sign_in_up_service.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sign_in_up_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$signInUpServiceHash() => r'1079556cf0560b82c5b7770350517848fa654359';
+
+/// See also [signInUpService].
+@ProviderFor(signInUpService)
+final signInUpServiceProvider = AutoDisposeProvider<SignInUpService>.internal(
+  signInUpService,
+  name: r'signInUpServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$signInUpServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef SignInUpServiceRef = AutoDisposeProviderRef<SignInUpService>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
