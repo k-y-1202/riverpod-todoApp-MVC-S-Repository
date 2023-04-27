957d583666957f9883416d362bbbae39c4e52da0
nobu <hiroki-pon@outlook.jp>
2023-04-27 14:11:32 +0900

    style.dart

--- lib/src/config/utils/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ColorName {
-  static const black = Color(0xFF333333);
-  static const grey = Color(0xFF9E9E9E);
-}
--- lib/src/config/utils/styles.dart
+++ lib/src/config/utils/styles.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:riverpod_todo_app_mvc_s_repository/src/config/utils/colors.dart';
 
+// textStyle
 class StyleName {
   static const title = TextStyle(
     fontSize: 16,
@@ -9,4 +9,19 @@ class StyleName {
     fontSize: 12,
     color: ColorName.grey,
   );
+}
+
+// color
+class ColorName {
+  static const black = Color(0xFF333333);
+  static const grey = Color(0xFF9E9E9E);
+}
+
+// padding
+class PaddingStyle {
+  static const v8 = SizedBox(height: 8);
+  static const v16 = SizedBox(height: 16);
+  static const v32 = SizedBox(height: 32);
+  static const h16 = SizedBox(width: 16);
+  static const h32 = SizedBox(width: 32);
 }
