04723534bdce84b60f1e7270d24f73eb779dd9f1
吉田光輝 <yoshidamitsuteru@yoshidamitsuterus-MacBook-Pro.local>
2023-05-01 09:53:27 +0900

    launch.json

--- lib/src/config/utils/pick/pick_web.dart
+++ lib/src/config/utils/pick/pick_web.dart
@@ -4,7 +4,7 @@ import 'package:image_picker_web/image_picker_web.dart';
 class Pick {
   Future<Uint8List?> pickFile() async {
     Uint8List? bytesFromPicker = await ImagePickerWeb.getImageAsBytes();
-    if (bytesFromPicker == null) return null;
+
     return bytesFromPicker;
   }
 }
