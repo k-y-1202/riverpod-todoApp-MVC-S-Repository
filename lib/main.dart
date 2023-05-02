da6b11a5a3655d75b4d23104aa73b87665d7fbef
nobu <hiroki-pon@outlook.jp>
2023-05-02 14:57:33 +0900

    providerの使い方のコメントを追加

--- lib/main.dart
+++ lib/main.dart
@@ -35,3 +35,46 @@ class MyApp extends ConsumerWidget {
     );
   }
 }
+
+
+// riverpod_generatorの使い方<作成編>
+// 1. riverpod_annotationをimport
+// 2. part 'ファイル名.g.dart';を書く
+/* 4. providerを作成
+<書き方1>
+@riverpod
+class クラス名 extends _$クラス名{
+  @override
+  stateで保持する型(hogeState, boolなど)  build() {
+    // ここに初期化処理を書く  
+  }
+
+  Future<void> getUser() async {
+    state = state.copyWith(isLoading: true);
+
+    final user = await ref.read(userServiceProvider.notifier).getUser();
+    state = state.copyWith(userName: user.userName),
+    );
+
+    state = state.copyWith(isLoading: false);
+  }
+
+  ※[注意] state, refはriverpod_generatorが自動で生成してくれる。5を実行するまではエラーが出るので注意。
+
+}
+  
+
+<書き方2>
+@riverpod
+FirebaseFirestore firestore(FirestoreRef ref) {
+  return FirebaseFirestore.instance;
+}
+*/ 
+// 5. flutter pub run build_runner build --delete-conflicting-outputsを実行
+
+
+// riverpod_generatorの使い方<使用編>
+// 1. ref.watch(Provider名)でProviderを呼び出す。stateにアクセスできる。
+// Provider名は作成したクラスの先頭を小文字＆Providerをつけた名前になる。
+// 例：class HogeController extends _$HogeController => hogeControllerProvider
+// 2. ref.read(Provider名.notifier)でProviderを呼び出す。stateにアクセスできない。関数を呼び出すときに使う。
